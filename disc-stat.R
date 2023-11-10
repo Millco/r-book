5d4e611c77ca8461cd011b28fd64d8b95c845156
Millco <roflomfgwtf@gmail.com>
2023-11-10 20:52:48 +0100

    Berechnung aus einem Kapitel Disc mit Andy Field

--- disc-stat.R
+++ disc-stat.R
@@ -16,3 +16,21 @@ bigbrother2
 chisq.test(bigbrother1)
 
 plot(bigbrother1)
+
+# 198/2529
+fbp <- c(22, 40, 53, 57, 93, 98, 103, 108, 116, 121, 252)
+
+fbp <- fbp[which(fbp < 250)]
+
+fbp.m <- sum(fbp) / length(fbp)
+for(i in 1:length(fbp)){
+  fbp.ss <- sum((fbp[i]-fbp.m)^2)
+}
+fbp.vr <- fbp.ss / length(fbp)
+fbp.sd <- sqrt(fbp.vr)
+
+cat('Der Mean der Facebookpeople beträgt', fbp.m,'.')
+cat('Die Varianz der Facebookpeople beträgt', fbp.vr, 'und die Standardabweichung beträgt', fbp.sd)
+
+
+?cat
